86056b7cb36008b10bad212ead43f23b30a11bcb
Joao-Comin <joaovictorcomin2005@gmail.com>
2024-11-27 16:02:46 -0300

    produtos layout feito

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -38,12 +38,12 @@ class DashboardController extends Controller
 
         //grafico 2 - categorias
 
-        $catData = Categoria::all();
+        $catData = Categoria::with('produtos')->get();
 
         //preparar o array
         foreach ($catData as $cat) {
             $catNome[] = " ' ".$cat->nome." ' ";
-            $catTotal[] = Produto::where('id_categoria', $cat->id)->count();
+            $catTotal[] = $cat->produtos->count();
         }
 
         //formatar
